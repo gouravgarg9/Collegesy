fd49a9eedb522eabf0e752e253eb8be1fd263d39
gouravgarg9 <97391212+gouravgarg9@users.noreply.github.com>
2023-08-14 18:22:36 +0530

    cookie set params change in logout

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -367,6 +367,7 @@ exports.logOutAllDevices = catchAsync(async (req,res,next)=>{
   res.cookie("jwt", "logged out", {
     expires: new Date(Date.now() + 1000),
     httpOnly: true,
+    sameSite: 'none'
   });
   res.status(200).json({
     status : 'success'
